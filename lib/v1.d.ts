71e1812383d6ee680c2be9c8e6817b6fb27e9211
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-04-19 07:38:01 +0100

    Update library distribution scripts

---
+++ lib/v1.d.ts
@@ -0,0 +1,3 @@
+import * as SurrealQL from "../dist/v1/index.d.ts";
+import { Value } from "../dist/v1/index.d.ts";
+export { Value, SurrealQL as default };
---
+++ lib/v2.d.ts
@@ -0,0 +1,3 @@
+import * as SurrealQL from "../dist/v2/index.d.ts";
+import { Value } from "../dist/v2/index.d.ts";
+export { Value, SurrealQL as default };
